d753478e2d2ab03d88b51c3a5b337f6d86a44681
pedro <pedromonteirobittencourt@gmail.com>
2025-03-26 15:11:19 -0300

    feat: refactor populate_tables to use Session rather than the engine directly

--- app/db/populate_tables.py
+++ app/db/populate_tables.py
@@ -1,32 +1,37 @@
 from sqlalchemy import insert
+from sqlalchemy.orm import Session
 
-from app.db.database import Base, engine
+from app.db.database import engine
 from app.models.user import User
 from app.models.expense import Expense
-from app.schemas.expense import ExpenseCategory
 from app.db.data import users, expenses
 
 
 def populate_users():
-    try:
-        stmt = insert(User).values(users)
-        with engine.connect() as conn:
-            result = conn.execute(stmt)
-            conn.commit()
-        print(f"Successfully added {result.rowcount} users")
-    except Exception as e:
-        print("Oops, something went wrong: " + str(e))
+    user_objects = [User(**user) for user in users]
+    with Session(engine) as session:
+        session.begin()
+        try:
+            session.add_all(user_objects)
+        except Exception as e:
+            session.rollback()
+            print("Oops, something went wrong: " + str(e))
+        else:
+            session.commit()
 
 
 def populate_expenses():
-    try:
-        stmt = insert(Expense).values(expenses)
-        with engine.connect() as conn:
-            result = conn.execute(stmt)
-            conn.commit()
-        print(f"Successfully added {result.rowcount} expenses")
-    except Exception as e:
-        print("Oops, something went wrong: " + str(e))
+    expense_objects = [Expense(**expense) for expense in expenses]
+    print(expense_objects)
+    with Session(engine) as session:
+        session.begin()
+        try:
+            session.add_all(expense_objects)
+        except Exception as e:
+            session.rollback()
+            print("Oops, something went wrong")
+        else:
+            session.commit()
 
 
 # populate_users()
