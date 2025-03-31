cd34793913ef4c310f14a5abea5caafad9e2dc22
pedro <pedromonteirobittencourt@gmail.com>
2025-03-31 15:33:15 -0300

    feat: updated user on db

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 # Add imports to ensure both models are fully loaded before SQLAlchemy tries to map the relationships
 from app.models.user import User
 from app.models.expense import Expense  # pylint: disable=unused-import
-from app.schemas.user import UserCreate
+from app.schemas.user import UserCreate, UserUpdate
 
 
 def get_users(db: Session) -> List[User]:
@@ -24,8 +24,22 @@ def get_user_id(db: Session, user_id: str) -> User:
 
 def create_user(db: Session, user_data: UserCreate) -> User:
     """Adds a user to the db"""
-    user = User(**user_data.model_dump())
-    db.add(user)
+    new_user = User(**user_data.model_dump())
+    db.add(new_user)
     db.commit()
-    db.refresh(user)
-    return user
+    db.refresh(new_user)
+    return new_user
+
+
+def update_user(db: Session, user_id: str, user_data: UserUpdate) -> User:
+    """Updates user info"""
+    # Get only fields that are provided
+    update_data = {k: v for k, v in user_data.model_dump().items() if v is not None}
+
+    # Query and update on db
+    db.query(User).filter(User.id == user_id).update(update_data)
+    db.commit()
+
+    # Retrieve updated user
+    updated_user = db.query(User).filter(User.id == user_id).first()
+    return updated_user
--- app/routes/users.py
+++ app/routes/users.py
@@ -2,9 +2,9 @@
 
 from fastapi import APIRouter
 
-from app.repositories.user import get_users, get_user_id, create_user
+from app.repositories.user import get_users, get_user_id, create_user, update_user
 from app.db.database import DbSession
-from app.schemas.user import UserCreate
+from app.schemas.user import UserCreate, UserUpdate
 
 router = APIRouter()
 
@@ -16,7 +16,7 @@ async def read_users(db: DbSession):
 
 
 @router.get("/users/{user_id}", tags=["users"])
-async def read_user_id(db: DbSession, user_id):
+async def read_user_id(db: DbSession, user_id: str):
     """Retrieves user by ID."""
     return get_user_id(db, user_id)
 
@@ -27,5 +27,14 @@ async def create_new_user(db: DbSession, user_data: UserCreate):
     try:
         validated_model = UserCreate.model_validate(user_data)
         return create_user(db, validated_model)
+    except Exception as e:
+        print(f"Exception: {e}")
+
+
+@router.patch("/users/{user_id}", tags=["users"])
+async def update_user_info(db: DbSession, user_id: str, user_data: UserUpdate):
+    """Updates a user"""
+    try:
+        return update_user(db, user_id, user_data)
     except Exception as e:
         print(f"Exception: {e}")
