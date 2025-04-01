1ab3a1dee9f00ab6cecd3fe7bc737878c62ff7a4
pedro <pedromonteirobittencourt@gmail.com>
2025-04-01 10:33:00 -0300

    feat: added try/except blocks and logging

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -8,27 +8,41 @@ from sqlalchemy.orm import Session
 from app.models.user import User
 from app.models.expense import Expense  # pylint: disable=unused-import
 from app.schemas.user import UserCreate, UserUpdate
+from app.exceptions import BadRequest, NotFound
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def get_users(db: Session) -> List[User]:
     """Retrieves all users from the db"""
     users = db.scalars(select(User)).all()
+    logger.info("Retrieved %d users.", len(users))
     return users
 
 
 def get_user_id(db: Session, user_id: str) -> User:
     """Retrieves user by id"""
     user = db.scalar(select(User).where(User.id == user_id))
+    if not user:
+        logger.warning("User %s not found.", user_id)
+        raise NotFound("User", user_id)
+    logger.info("Retrieved User %s, #%s", user.name, user_id)
     return user
 
 
 def create_user(db: Session, user_data: UserCreate) -> User:
     """Adds a user to the db"""
-    new_user = User(**user_data.model_dump())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+    try:
+        new_user = User(**user_data.model_dump())
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        logger.info("Created User %s, #%s.", new_user.name, new_user.id)
+        return new_user
+    except Exception as e:
+        logger.error("Failed to create user. Error: %s", str(e))
+        raise BadRequest("User")
 
 
 def update_user(db: Session, user_id: str, user_data: UserUpdate) -> User:
@@ -38,8 +52,8 @@ def update_user(db: Session, user_id: str, user_data: UserUpdate) -> User:
 
     # If no update data provided
     if not update_data:
-        # TODO: implement custom error # pylint: disable=W0511
-        pass
+        logger.warning("Couldn't update user #%s", user_id)
+        raise NotFound("User", user_id)
 
     # Query to update on db
     db.query(User).filter(User.id == user_id).update(update_data)
@@ -47,6 +61,7 @@ def update_user(db: Session, user_id: str, user_data: UserUpdate) -> User:
 
     # Retrieve updated user
     updated_user = get_user_id(db, user_id)
+    logger.info("Updated User %s, #%s", updated_user.name, user_id)
     return updated_user
 
 
@@ -55,3 +70,4 @@ def delete_user(db: Session, user_id: str) -> None:
     user = get_user_id(db, user_id)
     db.delete(user)
     db.commit()
+    logger.info("User %s deleted.", user.name)
