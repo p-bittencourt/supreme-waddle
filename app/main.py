cc90c9afa7116951774eb7d258d7d4362d9cb758
pedro <pedromonteirobittencourt@gmail.com>
2025-04-01 19:49:04 -0300

    feat: order LogLevels to match pythons logging documentation

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from app.routes import users, expenses
 from .logging import configure_logging, LogLevels
 
 
-configure_logging(LogLevels.error)
+configure_logging(LogLevels.info)
 
 app = FastAPI(
     title="Expense Tracker API",
